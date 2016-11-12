130a3bdd82042463d93276391972410936c74403
Rey Tanada <reyvtanada@gmail.com>
2016-11-12 12:36:15 -0800

    Testing config issues

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      # TODO send message
-      flash[:notice] = "Message sent from #{@contact.name}"
+      UserMailer.contact_email(@contact).deliver_now
+      flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
     else
       render :new
@@ -22,3 +22,6 @@ class ContactsController < ApplicationController
   end
 
 end
+
+puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
+puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
