a2699fb9945d79d6330f7127d7dbacce216fb2c8
Rey Tanada <reyvtanada@gmail.com>
2016-11-12 18:44:24 -0800

    Fix production env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,15 +79,15 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: "learned-rails-example-app.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["email_provider_username"],
-    password: ENV["email_provider_password"]
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENGRID_PASSWORD"]
   }
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => ENV["domain_name"] }
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
